e01d69bd25881824f1d3f257486ab2f0bca1860e
Łukasz <lukasz.f.d@gmail.com>
2021-09-29 20:44:26 +0200

    +AutoMapper

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using AutoMapper;
 
 namespace PlatformService
 {
@@ -30,7 +31,9 @@ namespace PlatformService
         {
             services.AddDbContext<AppDbContext>(opt => 
             opt.UseInMemoryDatabase("InMem"));
+            services.AddScoped<IPlatformRepo,PlatformRepo>();
             services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
@@ -57,6 +60,8 @@ namespace PlatformService
             {
                 endpoints.MapControllers();
             });
+
+            PrepDb.PrepPopulation(app, env.IsProduction());
         }
     }
 }
